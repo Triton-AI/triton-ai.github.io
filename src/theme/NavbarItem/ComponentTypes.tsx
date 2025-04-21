1cf73e09c308bfe3a202c988bbde32742b2f9087
kevshin2002 <kevshin2002@gmail.com>
2025-04-20 20:33:35 -0700

    [Infrastructure] Initial commit

---
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,11 @@
+/**
+ * SWIZZLED VERSION: 3.5.2
+ * REASONS:
+ *  - Extent ComponentTypes with custom-WalletConnectButton
+ */
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+
+
+export default {
+  ...ComponentTypes,
+};
