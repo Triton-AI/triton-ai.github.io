fbdb174bb59fb3ef61864b54866497592a3587e0
WinstonHChou <winston.h.chou@gmail.com>
2025-04-30 00:28:06 -0700

    fix website.sh, remove `  --privileged \   --net=host \`

--- website.sh
+++ website.sh
@@ -4,14 +4,12 @@ docker run \
   --name tai-website-dev\
   -it \
   --rm \
-  --privileged \
-  --net=host \
   -p 3000:3000 \
   -v "$(pwd):/app" \
   -w /app \
   tai-website bash -c "
     apt-get update && \
-    apt-get install -y vim nano neovim curl git && \
+    apt-get install -y vim nano neovim curl git net-tools && \
     npm install -g pnpm && \
     pnpm install && \
     bash"
